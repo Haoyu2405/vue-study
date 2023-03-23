87b454fb302f3a5493ebd2e05f138a52bccbd91e
Wenyu520521 <2584107537@qq.com>
2023-03-23 20:57:30 +0800

    v-html和v-text

---
+++ src/10_v-html和v-text.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p v-text="str">{{ 12424 }}</p>
+    <p v-html="str"></p>
+    <!-- 注意：v-text和v-html都会覆盖插值表达式 -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      str: '<span>1245</span>',
+    }
+  },
+}
+</script>
+<style scoped></style>
