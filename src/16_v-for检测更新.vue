2d9e63992969bb22ca7c1e7cbe4e976f65881888
Wenyu520521 <2584107537@qq.com>
2023-03-24 16:56:32 +0800

    v-for检测更新

---
+++ src/16_v-for检测更新.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in arr" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="reverseBtn">翻转数组</button>
+    <button @click="spliceBtn">截取数组</button>
+    <button @click="updateBtn">更新数组</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 3, 7, 8, 5],
+    }
+  },
+  methods: {
+    // 使原数组发生改变的方法，v-for可以检测到，使页面发生更新
+    // 反之，v-for检测不到，页面不发生更新
+    reverseBtn() {
+      this.arr.reverse()
+    },
+    spliceBtn(){
+      // splice方法会改变原始数组
+      this.arr.splice(0,3)
+    },
+    updateBtn(){
+      // 更新数组的某个元素，v-for检测不到页面没有更新
+      this.arr[0] = 1000
+      // 解决方法：this.$set()
+      /**
+       * 参数1：目标对象
+       * 参数2：更新的位置
+       * 参数3：要更新的值
+       */
+      this.$set(this.arr,0,1000)
+    }
+  },
+}
+</script>
+<style scoped></style>
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(item, index) in arr" :key="item">
-        <span>{{ item }}</span>
-        <button @click="del(index)">删除</button>
-      </li>
-    </ul>
-    <button @click="add">生成</button>
+
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      arr: [1, 3, 6],
-    }
+  data () {
+    return {}
   },
-  methods: {
-    add(){
-      this.arr.push(Math.floor(Math.random()*20))
-    },
-    del(index){
-      this.arr.splice(index,1)
-    }
-  },
+  methods: {},
 }
+
 </script>
-<style scoped></style>
+<style scoped>
+</style>
