5c1f824f43d4a6b2b8f0344a5db31a1dd9be5890
Wenyu520521 <2584107537@qq.com>
2023-03-25 22:08:59 +0800

    计算属性_完整写法

---
+++ src/26_计算属性_完整写法.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <span>姓名：</span>
+    <input type="text"  v-model="full">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {},
+  /**
+   * 计算属性也是变量，当需要给计算属性赋值时，使用计算属性的完整写法
+   * computed:{
+   *  "计算属性名"(){},  // 简写
+   *  "计算属性名":{     // 完整写法
+   *      set(值){
+   *      
+   *      },
+   *      get(){
+   *          return 值
+   *      }
+   *   }
+   * }
+   */
+  computed:{
+    full:{
+      // 给full赋值触发set方法
+      set(val){
+        console.log(val);
+      },
+      // 使用full的值触发get方法
+      get(){
+        return this.set
+        // return '豪雨'
+      }
+    }
+  }
+}
+
+</script>
+<style scoped>
+</style>
