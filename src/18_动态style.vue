8cf46c008145f9d0b4c2111f1d2bf08ecf0a35f2
Wenyu520521 <2584107537@qq.com>
2023-03-25 08:26:39 +0800

    动态style

---
+++ src/18_动态style.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+<p :style="{color:colorStr}">value变量的值，将被赋予给css属性的key生效</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      colorStr:'red'
+    }
+  },
+  methods: {},
+}
+
+</script>
+<style scoped>
+</style>
