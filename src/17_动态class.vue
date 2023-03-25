60c54394578aea8e5f4b2a8a707c168ac109d6e0
Wenyu520521 <2584107537@qq.com>
2023-03-25 08:22:04 +0800

    动态class

---
+++ src/17_动态class.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- :class="{类名：布尔值}" -->
+    <p :class="{ redStr: bool }">值为true，key作为类名生效</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bool: true,
+    }
+  },
+  methods: {},
+}
+</script>
+<style scoped>
+.redStr {
+  color: red;
+}
+</style>
