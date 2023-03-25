30b2f34bd6a12da76a11baeee111b3c05e24bc0b
Wenyu520521 <2584107537@qq.com>
2023-03-25 15:33:55 +0800

    过滤器

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
+  <!-- 
+    过滤器：传入一个值，处理后返回另外一个值
+   * 定义语法：
+     main.js - Vue.filter('过滤器名字',函数体)
+     某个.vue文件- filters:{'过滤器名字'：函数体}
+   * 使用语法：
+    Vue变量 | 过滤器名字
+   * 注意事项：
+    只能使用在 插值表达式 / 动态属性
+   -->
   <div>
-
+<p>原来的msg：{{ msg }}</p>
+<p>翻转后的msg：{{ msg | reverse }}</p>
+<p :title="msg | toUpper">鼠标长停</p>
   </div>
 </template>
 
-<script>
+<script> 
 export default {
   data () {
-    return {}
+    return {
+      msg:'Hello,World!'
+    }
   },
-  methods: {},
+  methods: {
+    
+  },
+  /**
+   * 过滤器（局部）
+   * filters:{ 
+   *  过滤器名(val){
+   *   return 处理后的值
+   * }
+   * }
+   */
+  filters:{
+      toUpper(val){
+        return val.toUpperCase()
+      }
+    }
 }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "bootstrap/dist/css/bootstrap.css"
 
 Vue.config.productionTip = false // 在控制台有一句提示消息
 
+// 过滤器（全局） Vue.filter('过滤器名',函数体)
+Vue.filter('reverse',val => val.split('').reverse().join(''));
+
 new Vue({ // 开始实例化vue
   render: h => h(App), // 准备渲染App页面
 }).$mount('#app') // 渲染到index.html文件里id叫app的标签上
