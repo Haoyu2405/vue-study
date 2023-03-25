277bb7c89bd92d0352acdd9f76c546e89daab38d
Wenyu520521 <2584107537@qq.com>
2023-03-25 21:29:03 +0800

    计算属性,计算属性的缓存机制

---
+++ src/24_计算属性_缓存机制.vue
@@ -0,0 +1,37 @@
+<template>
+ <!-- 计算属性的好处是？
+  * 带缓存，只要依赖项不变，都直接从缓存取
+  * 依赖项改变函数自动自动执行并重新缓存
+-->
+  <div>
+    <p>{{ reverseComputed }}</p>
+    <p>{{ reverseComputed }}</p>
+    <p>{{ reverseComputed }}</p>
+    <p>{{ reverseMethod() }}</p>
+    <p>{{ reverseMethod() }}</p>
+    <p>{{ reverseMethod() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello,Vue',
+    }
+  },
+  methods: {
+    reverseMethod() {
+      console.log('函数执行了')
+      return this.msg.split('').reverse().join('')
+    },
+  },
+  computed: {
+    reverseComputed() {
+      console.log('计算属性执行了')
+      return this.msg.split('').reverse().join('')
+    },
+  },
+}
+</script>
+<style scoped></style>
