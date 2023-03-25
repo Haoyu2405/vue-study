34fcac59e06ffa0ef3c7d82d1dd704e27e8400f9
Wenyu520521 <2584107537@qq.com>
2023-03-25 15:48:52 +0800

    多个过滤器，过滤器传参

---
+++ src/21_过滤器传参,多个过滤器.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>原来的msg：{{ msg }}</p>
+    <!-- 过滤器传参 -->
+    <p>翻转且传参后的msg：{{ msg | reverse('|') }}</p>
+    <!-- 多个过滤器 -->
+    <p :title="msg | reverse('|') | toUpper">鼠标长停</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello,World!',
+    }
+  },
+  methods: {},
+  filters: {
+    toUpper(val) {
+      return val.toUpperCase()
+    },
+  },
+}
+</script>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import "bootstrap/dist/css/bootstrap.css"
 Vue.config.productionTip = false // 在控制台有一句提示消息
 
 // 过滤器（全局） Vue.filter('过滤器名',函数体)
-Vue.filter('reverse',val => val.split('').reverse().join(''));
+// Vue.filter('reverse',val => val.split('').reverse().join(''));
+
+// 过滤器传参
+Vue.filter('reverse',(val,s)=>{
+  return val.split('').reverse().join(s)
+})
 
 new Vue({ // 开始实例化vue
   render: h => h(App), // 准备渲染App页面
