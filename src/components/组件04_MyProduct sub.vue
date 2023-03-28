8c9d652c78b98c19fef102ff6c812f5fd8001818
Wenyu520521 <2584107537@qq.com>
2023-03-28 15:39:39 +0800

    组件通信_父向子_单向数据流

---
+++ src/components/组件04_MyProduct sub.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="my-product">
+    <h3>标题: {{ title }}</h3>
+    <p>价格: {{ price }}元</p>
+    <p>{{ intro }}</p>
+    <button @click="subFn">宝刀，砍一元</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'price', 'intro'],
+  methods:{
+    subFn(){
+      // eslint-disable-next-line vue/no-mutating-props
+      /**
+       *  错误：单向数据流 （从父到子的数据流向）
+       *  1.子组件改父组件传入的数据不通知父亲，会导致数据的不一致性
+       *  2.vue规定props“本身”只读的（不允许重新赋值）
+       */
+      // this.price = this.price - 1
+    }
+  }
+}
+</script>
+
+<style>
+.my-product {
+  width: 400px;
+  padding: 20px;
+  border: 2px solid #000;
+  border-radius: 5px;
+  margin: 10px;
+}
+</style>
