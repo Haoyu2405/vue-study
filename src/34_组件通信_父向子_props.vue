f9f23cd81eff7bab0ae9cc294f86158da06da7bc
Wenyu520521 <2584107537@qq.com>
2023-03-28 15:05:56 +0800

    组件通信，父向子，props

---
+++ src/34_组件通信_父向子_props.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- 
+      * 组件通信，父(App.vue)传子(MyProduct.vue)  (谁被引入，谁是子)
+      * 1.子组件里准备好参数 props: ['title', 'price', 'intro'] title、price、intro都是自定义的变量名
+      * 2.父组件导入子组件
+      * 3.父组件接收参数并赋值使用
+     -->
+<Product title="超好7的口水鸭" price="50" intro="开业大酬宾,全场8折!"></Product>
+<Product title="好可爱的可爱多" price="5201314---++++" intro="三生三世十里桃花"></Product>
+<Product title="北京烤鸭" price="299" :intro="str"></Product>
+  </div>
+</template>
+
+<script>
+import Product from './components/组件03_MyProduct.vue'
+export default {
+  data () {
+    return {
+      str:'领导不在公司，全场一折!'
+    }
+  },
+  methods: {},
+  components:{
+    // 当key和value相同时，可以简写
+    // Product: Product -> Product
+    Product
+  }
+}
+
+</script>
+<style scoped>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,17 @@
 <template>
-  <!-- 
-      * 组件是什么？
-        可复用的vue实例，封装了标签，样式，JS
-      * 什么时候封装组件？
-        遇到重复标签可复用的时候
-      * 组件的好处？
-        各自独立，互不影响，便于复用
-   -->
-  <div id="app">
-    <h4>案例：折叠面板</h4>
-    <!-- 4.使用组件(局部) -->
-    <!-- <Pannel></Pannel>(对应2） -->
-    <Pannel></Pannel>
-    <!-- 4.使用组件(全局) -->
-    <PannelG></PannelG>
+  <div>
+
   </div>
 </template>
 
 <script>
-// 局部组件 （使用较多）
-// 1.创建组件.vue文件
-// 2.引入组件
-// import Pannel(对应1) from './components/组件01_Pannel.vue'
-// 3.注册组件
-// components:{
-//  Pannel（对应2）: Pannel（对应1）,
-// } 
-import Pannel from './components/组件01_Pannel.vue'
 export default {
-  components: {
-    Pannel: Pannel,
-  },
-  data() {
+  data () {
     return {}
   },
   methods: {},
 }
+
 </script>
-<style lang="less" scoped>
-body {
-  background-color: #ccc;
-  #app {
-    width: 400px;
-    margin: 20px auto;
-    background-color: #fff;
-    border: 4px solid blueviolet;
-    border-radius: 1em;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-    padding: 1em 2em 2em;
-    h3 {
-      text-align: center;
-    }
-  }
-}
+<style scoped>
 </style>
