e8f4d21d29a545084907def28bb438cff108a3ef
Wenyu520521 <2584107537@qq.com>
2023-04-12 11:08:31 +0800

    组件缓存下，activated和deactivated得到、失去激活状态

--- src/components/01/UserName.vue
+++ src/components/01/UserName.vue
@@ -19,6 +19,15 @@ export default {
   destroyed() {
     console.log('UserName组件销毁')
   },
+  // 组件缓存下多了两个钩子函数
+  // activated - 得到激活状态
+  // deactivated - 失去激活状态
+  activated() {
+    console.log('UserName组件激活')
+  },
+  deactivated() {
+    console.log('UserName组件失去激活')
+  },
   data() {
     return {}
   },
